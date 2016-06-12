7069b195c4e55c5592c0a6d7a07e452bbcc22b3e
Geor Kasapidi <geor.kasapidi@icloud.com>
2016-06-12 13:21:32 +0300

    fix ios 8 crash and app window setup

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -14,17 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        let jsonDemoController = JSONDemoViewController()
+        let jsonDemoController = JSONDemoViewController(nibName: "JSONDemoViewController", bundle: nil)
         jsonDemoController.tabBarItem = UITabBarItem(tabBarSystemItem: .Contacts, tag: 0)
         
-        let imageDemoController = ImageDemoViewController()
+        let imageDemoController = ImageDemoViewController(nibName: "ImageDemoViewController", bundle: nil)
         imageDemoController.tabBarItem = UITabBarItem(tabBarSystemItem: .Favorites, tag: 1)
         
         let tabsController = UITabBarController()
         tabsController.viewControllers = [UINavigationController(rootViewController: jsonDemoController),
                                           UINavigationController(rootViewController: imageDemoController)]
         
-        window = UIWindow()
+        window = NSProcessInfo.processInfo().isOperatingSystemAtLeastVersion(NSOperatingSystemVersion(majorVersion: 9, minorVersion: 0, patchVersion: 0)) ? UIWindow() : UIWindow(frame: UIScreen.mainScreen().bounds)
         window!.rootViewController = tabsController
         window!.makeKeyAndVisible()
         
