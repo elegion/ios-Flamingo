bbc20d5685e61c81b553e721c234284bfa9b2ecd
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2017-09-25 12:35:34 +0300

    Add missing HTTP methods

--- Source/URLConvertible.swift
+++ Source/URLConvertible.swift
@@ -13,6 +13,11 @@ public enum HTTPMethod: String {
     case get = "GET"
     case post = "POST"
     case patch = "PATCH"
+    case delete = "DELETE"
+    case put = "PUT"
+    case head = "HEAD"
+    case options = "OPTIONS"
+    case update = "UPDATE"
     
 }
 
