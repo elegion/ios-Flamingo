f1aa8a1a90868fb0ab1345833ef58d8626b23c48
Nikolay Ischuk <ischuknn@gmail.com>
2018-03-15 01:02:29 +0300

    Fix older tests

--- Framework/FlamingoTests/NetworkClient/NetworkClientStubs.swift
+++ Framework/FlamingoTests/NetworkClient/NetworkClientStubs.swift
@@ -15,6 +15,10 @@ private class StubsManagerMock: StubsManager {
 
     internal var hasMockAnswer: Bool = false
 
+    var error: Swift.Error?
+
+    public var notFoundStubBehavior: NotFoundStubBehavior = .giveError
+
     func add(_ key: RequestStub, stub: ResponseStub) {
 
     }
@@ -37,20 +41,29 @@ private class StubsManagerMock: StubsManager {
 
         let url = URL(fileURLWithPath: "")
         if self.hasMockAnswer {
-            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: [:])
-            return (response_body().data(using: .utf8), response, nil)
+            if let error = error {
+                return (nil, nil, error)
+            } else {
+                let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: [:])
+                return (response_body().data(using: .utf8), response, nil)
+            }
         } else {
-            return (nil, nil, StubError.stubClientError(.stubNotFound))
+            switch notFoundStubBehavior {
+            case .giveError:
+                return (nil, nil, StubsError.stubClientError(.stubNotFound))
+            case .useRealClient:
+                return nil
+            }
         }
     }
-}
 
-private func api_method() -> String {
-    return "index.html"
-}
+    private func api_method() -> String {
+        return "index.html"
+    }
 
-private func response_body() -> String {
-    return "{\"field\":\"value\"}"
+    private func response_body() -> String {
+        return "{\"field\":\"value\"}"
+    }
 }
 
 private struct TestModel: Decodable, Equatable {
@@ -63,7 +76,7 @@ private struct TestModel: Decodable, Equatable {
 
 private struct TestRequest: NetworkRequest {
     var URL: URLConvertible {
-        return api_method()
+        return "index.html"
     }
 
     var responseSerializer: CodableJSONSerializer<TestModel> {
@@ -105,29 +118,23 @@ class NetworkClientStubs: XCTestCase {
         client.disableStubs()
     }
 
-//    public func test_getStubOnNotConfiguredClient_expectedError() {
-//        let expectation = self.expectation(description: #function)
-//        let client = self.client
-//        client.enableStubs()
-//
-//        let request = TestRequest()
-//        client.sendRequest(request) {
-//            result, _ in
-//
-//            guard let errorInRes = result.error,
-//                case let Flamingo.Error.networkClientError(error) = errorInRes else {
-//                    XCTFail("Wrong error!")
-//                    expectation.fulfill()
-//                    return
-//            }
-//
-//            XCTAssertTrue(true)
-//
-//            expectation.fulfill()
-//        }
-//
-//        self.waitForExpectations(timeout: 5, handler: nil)
-//    }
+    public func test_getStubOnNotConfiguredClient_expectedError() {
+        let expectation = self.expectation(description: #function)
+        let client = self.client
+        client.enableStubs()
+
+        let request = TestRequest()
+        client.sendRequest(request) {
+            result, _ in
+
+            XCTAssertNotNil(result.error)
+            XCTAssertTrue(result.error is Swift.DecodingError, "\(result.error?.localizedDescription ?? "")")
+
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
 
     public func test_getStub_expectedResponse() {
         let expectation = self.expectation(description: #function)
@@ -148,24 +155,45 @@ class NetworkClientStubs: XCTestCase {
         self.waitForExpectations(timeout: 5, handler: nil)
     }
 
-//    public func test_changingToRealClientOnNotExistsStub_expectedSwiftError() {
-//        let expectation = self.expectation(description: #function)
-//        let client = self.configuredClient
-//        client.enableStubs()
-//        (client.StubsManager as? StubsManagerMock)?.hasMockAnswer = false
-//
-//        let request = TestRequest()
-//        client.sendRequest(request) {
-//            _, context in
-//
-//            XCTAssertNotNil(context?.error)
-//            XCTAssertTrue(context?.error is Swift.DecodingError, "\(context?.error?.localizedDescription ?? "")")
-//
-//            expectation.fulfill()
-//        }
-//
-//        self.waitForExpectations(timeout: 5, handler: nil)
-//    }
+    public func test_changingToRealClientOnNotExistsStub_expectedSwiftError() {
+        let expectation = self.expectation(description: #function)
+        let client = self.configuredClient
+        client.enableStubs()
+        (client.stubsManager as? StubsManagerMock)?.notFoundStubBehavior = .useRealClient
+        (client.stubsManager as? StubsManagerMock)?.hasMockAnswer = false
+
+        let request = TestRequest()
+        client.sendRequest(request) {
+            result, _ in
+
+            XCTAssertNotNil(result.error)
+            XCTAssertTrue(result.error is Swift.DecodingError, "\(result.error?.localizedDescription ?? "")")
+
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    public func test_expectingStubsError() {
+        let expectation = self.expectation(description: #function)
+        let client = self.configuredClient
+        client.enableStubs()
+        (client.stubsManager as? StubsManagerMock)?.hasMockAnswer = false
+        (client.stubsManager as? StubsManagerMock)?.notFoundStubBehavior = .giveError
+
+        let request = TestRequest()
+        client.sendRequest(request) {
+            result, _ in
+
+            XCTAssertNotNil(result.error)
+            XCTAssertTrue(result.error is StubsError, "\(String(describing: result.error?.localizedDescription))")
+
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
 
     public func test_gettingResponseOnExistingStub_expectedResponse() {
         let expectation = self.expectation(description: #function)
