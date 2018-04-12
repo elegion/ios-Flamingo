26d57fd951f082e7e90034b406d5cda98fc4518d
funkill2 <funkill2@gmail.com>
2018-04-12 10:58:58 +0300

    added checking preparing parameters and converting url by network client

--- Framework/FlamingoTests/NetworkClient/NetworkClientTestCase.swift
+++ Framework/FlamingoTests/NetworkClient/NetworkClientTestCase.swift
@@ -9,6 +9,13 @@
 import XCTest
 import Flamingo
 
+private struct Consts {
+    static let headers = [
+        (name: "Accept", value: "text/*;q=0.3, text/html;q=0.7, text/html;level=1, text/html;level=2;q=0.4, */*;q=0.5"),
+        (name: "Content-Language", value: "en, asc, ru"),
+    ]
+}
+
 class NetworkClientTestCase: XCTestCase {
 
     func test_parametersEncodingByParametersTuple() {
@@ -36,11 +43,59 @@ class NetworkClientTestCase: XCTestCase {
             XCTFail(error.localizedDescription)
         }
     }
+
+    func test_prepareUrlRequest() {
+        let client = NetworkDefaultClientStubs.defaultForTest()
+        client.isUseCustomizedCustomHeaders = true
+        var networkRequest = RequestWithParamsTuple()
+        networkRequest.customCachePolicy = .useProtocolCachePolicy
+
+        do {
+            let request = try client.urlRequest(from: networkRequest)
+            var headers = Consts.headers
+            headers.append(contentsOf: NetworkDefaultClientStubs.Consts.headers)
+
+            headers.forEach {
+                name, expectedValue in
+
+                let actualValue = request.value(forHTTPHeaderField: name)
+                XCTAssertEqual(expectedValue, actualValue)
+            }
+            XCTAssertEqual(request.cachePolicy, networkRequest.cachePolicy)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
+    func test_throwingErrorOnBadURL() {
+        let expectation = self.expectation(description: #function)
+        let client = NetworkDefaultClientStubs.defaultForTest()
+        var networkRequest = RequestWithParamsTuple()
+        networkRequest.customBaseUrl = FailURLConvertible()
+        let operation = client.sendRequest(networkRequest) {
+            result, _ in
+
+            XCTAssertFalse(result.isSuccess)
+            XCTAssertNotNil(result.error)
+
+            expectation.fulfill()
+        }
+
+        XCTAssertNil(operation)
+
+        wait(for: [expectation], timeout: 10)
+    }
 }
 
 private struct RequestWithParamsTuple: NetworkRequest {
+    var customURL: URLConvertible = "index.htm"
     var URL: URLConvertible {
-        return "index.htm"
+        return customURL
+    }
+
+    var customBaseUrl: URLConvertible = "http://example.com"
+    var baseURL: URLConvertible? {
+        return customBaseUrl
     }
 
     var parameters: [String: Any]?
@@ -58,4 +113,26 @@ private struct RequestWithParamsTuple: NetworkRequest {
     var responseSerializer: StringResponseSerializer {
         return StringResponseSerializer()
     }
+
+    var headers: [String: String?]? {
+        return [
+            Consts.headers[0].name: Consts.headers[0].value,
+            Consts.headers[1].name: Consts.headers[1].value,
+        ]
+    }
+
+    var customCachePolicy: URLRequest.CachePolicy?
+    var cachePolicy: URLRequest.CachePolicy? {
+        return customCachePolicy
+    }
+}
+
+private struct FailURLConvertible: URLConvertible {
+    func asURL() throws -> URL {
+        throw TestURLConvertibleError.failURLConvertible
+    }
+}
+
+private enum TestURLConvertibleError: Swift.Error {
+    case failURLConvertible
 }
